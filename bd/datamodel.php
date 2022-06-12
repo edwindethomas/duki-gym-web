6b24874e253c73920ad02ecbd0b555283eabc1b9
Edwin De Thomas <L18020768@veracruz.tecnm.mx>
2022-06-12 15:23:12 -0500

    a

--- bd/datamodel.php
+++ bd/datamodel.php
@@ -1,5 +1,5 @@
 <?php
-require_once "./conexion.php";
+require_once "../bd/conexion.php";
 
 function InsertarEntrenador($nombrecompleto, $usuario, $password, $telefono)
 {
--- indexentrenador.php
+++ indexentrenador.php
@@ -1,9 +1,13 @@
 <?php
-require_once "./bd/datamodel.php";
+#require_once "./bd/conexion.php";
+#require_once "./bd/datamodel.php";
 
 session_start(); #Activo la persistencia #Siempre será la primera instruccion ejecutable
+
+echo $_SESSION['Logueado'];
 if(isset($_SESSION['Logueado'])){
     if($_SESSION['Logueado']){
+        echo "Prueba";
         header("Location: indexcliente.php");
     }
 }
